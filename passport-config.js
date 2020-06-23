a87bc26318f36e1274df2154cb1014f7e78f4038
Satyaki Suman <satyaki.suman@gmail.com>
2020-06-23 19:44:13 +0530

    commit

--- passport-config.js
+++ passport-config.js
@@ -1,13 +1,26 @@
 const LocalStrategy =  require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
-function initialize(passport) {
-    const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
         if(user ==  null){
             return done(null, false, { message: 'Invalid credentials'})
         }
+        try {
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user)
+            }else {
+                return done(null, false, { message: 'Invalid Credentials'} )
+            }
+        } catch (error) {
+            return done(error)
+        }
     }
-    passport.use(new LocalStrategy({ usernameField: 'email'}), authenticateUser)
-    passport.serializeUser((user, done) => { })
-    passport.serializeUser((id, done) => { })
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => done(null, getUserById(id))
+    )
 }
+
+module.exports = initialize
